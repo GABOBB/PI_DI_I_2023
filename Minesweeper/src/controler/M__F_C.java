e60abd5bfb35c3e1f704082d0ee4b8d7060e322a
GABOBB <gabbol2002@gmail.com>
2023-03-12 21:27:09 -0600

    primer acercamiento a la interfaz v0.1.0

---
+++ Minesweeper/src/controler/M__F_C.java
@@ -0,0 +1,194 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package controler;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author Gabriel
+ */
+public class M__F_C implements Initializable {
+
+    @FXML
+    private Button g_m;
+    @FXML
+    private ImageView image_g_m;
+    @FXML
+    private Button b0_0;
+    @FXML
+    private Button b0_1;
+    @FXML
+    private Button b0_2;
+    @FXML
+    private Button b0_3;
+    @FXML
+    private Button b0_7;
+    @FXML
+    private Button b0_6;
+    @FXML
+    private Button b0_5;
+    @FXML
+    private Button b0_4;
+    @FXML
+    private Button b1_4;
+    @FXML
+    private Button b1_5;
+    @FXML
+    private Button b1_6;
+    @FXML
+    private Button b1_7;
+    @FXML
+    private Button b1_3;
+    @FXML
+    private Button b1_2;
+    @FXML
+    private Button b1_1;
+    @FXML
+    private Button b1_0;
+    @FXML
+    private Button b2_4;
+    @FXML
+    private Button b2_5;
+    @FXML
+    private Button b2_6;
+    @FXML
+    private Button b2_7;
+    @FXML
+    private Button b2_3;
+    @FXML
+    private Button b2_2;
+    @FXML
+    private Button b2_1;
+    @FXML
+    private Button b2_0;
+    @FXML
+    private Button b3_4;
+    @FXML
+    private Button b3_5;
+    @FXML
+    private Button b3_6;
+    @FXML
+    private Button b3_7;
+    @FXML
+    private Button b3_3;
+    @FXML
+    private Button b3_2;
+    @FXML
+    private Button b3_1;
+    @FXML
+    private Button b3_0;
+    @FXML
+    private Button b4_4;
+    @FXML
+    private Button b4_5;
+    @FXML
+    private Button b4_6;
+    @FXML
+    private Button b4_7;
+    @FXML
+    private Button b4_3;
+    @FXML
+    private Button b4_2;
+    @FXML
+    private Button b4_1;
+    @FXML
+    private Button b4_0;
+    @FXML
+    private Button b5_4;
+    @FXML
+    private Button b5_5;
+    @FXML
+    private Button b5_6;
+    @FXML
+    private Button b5_7;
+    @FXML
+    private Button b5_3;
+    @FXML
+    private Button b5_2;
+    @FXML
+    private Button b5_1;
+    @FXML
+    private Button b5_0;
+    @FXML
+    private Button b6_4;
+    @FXML
+    private Button b6_5;
+    @FXML
+    private Button b6_6;
+    @FXML
+    private Button b6_7;
+    @FXML
+    private Button b6_3;
+    @FXML
+    private Button b6_2;
+    @FXML
+    private Button b6_1;
+    @FXML
+    private Button b6_0;
+    @FXML
+    private Button b7_4;
+    @FXML
+    private Button b7_5;
+    @FXML
+    private Button b7_6;
+    @FXML
+    private Button b7_7;
+    @FXML
+    private Button b7_3;
+    @FXML
+    private Button b7_2;
+    @FXML
+    private Button b7_1;
+    @FXML
+    private Button b7_0;
+    
+    private Boolean begin = false;
+    private Boolean ga_mo = false;
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void game_mode(ActionEvent event) {
+        if(!this.begin){
+            
+            //insertar cambio de imagen
+        }else{
+            //insertar cambio de imagen
+        }
+        this.ga_mo=!this.ga_mo;
+        this.begin=!this.begin;
+    }
+
+    @FXML
+    private void cords_selected(MouseEvent e) {
+        Button x = (Button) e.getSource();
+        
+        if ("PRIMARY".equals(e.getButton().toString())){
+            System.out.println("der ");
+        }
+        if ("SECONDARY".equals(e.getButton().toString())){
+            System.out.println("iz ");
+        }
+        
+    }
+    
+}
---
+++ Minesweeper/src/controler/MainClass.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package controler;
+
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class MainClass extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainClass.class.getResource("/visual/Main_FXML.fxml"));
+            Pane ventana = (Pane) loader.load();//se carga la ventana
+            Scene scene = new Scene(ventana);//se carga la escena
+            primaryStage.setScene(scene);//se setea la escena
+            primaryStage.show();//se ense;a la escena
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
